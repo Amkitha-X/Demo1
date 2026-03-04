65875ef29d2093d8625869a999c51352726c7d41
Amkhitha-X <224078705@ufh.ac.za>
2026-03-04 17:27:42 +0200

    Force

---
+++ src/Demo1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Demo1 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name;
+
+        System.out.print("Enter name: ");
+        name = scanner.next();
+
+        System.out.println("Name: " +name);
+
+
+       System.out.println("Hello Kitts");
+
+
+    }
+}
